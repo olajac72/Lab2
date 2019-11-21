dcb2eda8a2616d2e14ab1589198362f36557fec8
Ola Jacobsson <ola.jacobsson@bredband.net>
2019-11-21 20:26:58 +0100

    Changed totalsum and column alignment

--- 2Darray/2Darray.cpp
+++ 2Darray/2Darray.cpp
@@ -4,16 +4,19 @@
 #include <iostream>
 #include <cstdlib>
 #include <time.h>
+#include <iomanip>
+
 using namespace std;
 
 void array_rows_cols(int calcarray[][10], int r, int c)
 {
-	int rowsum, colsum[10], totsum;
+	int rowsum, colsum[10], totsum, rowsums[10];
 	rowsum = 0;
 
 	for (int reset = 0; reset < 10; reset++)
 	{
 		colsum[reset] = 0;
+		rowsums[reset] = 0;
 	}
 
 	totsum = 0;
@@ -21,22 +24,32 @@ void array_rows_cols(int calcarray[][10], int r, int c)
 	for (int i = 0; i < r; i++) {
 		for (int j = 0; j < c; j++)
 		{
-			totsum = totsum + calcarray[i][j];
+			//totsum = totsum + calcarray[i][j];
 			rowsum = rowsum + calcarray[i][j];
 			colsum[j] = colsum[j] + calcarray[i][j];
-			cout << calcarray[i][j] << " ";
+			cout  << calcarray[i][j] << setw(5);
 		}
 		cout << rowsum << endl;
-
+		rowsums[i] = rowsum;
 		rowsum = 0;
+	}
+
+	for (int i = 0; i < r; i++)
+	{
+		totsum += rowsums[i];
+	}
+
+	for (int i = 0; i < c; i++)
+	{
+		totsum += colsum[i];
 	}
 
 	for (int colsums = 0; colsums < c; colsums++)
 	{
-		cout << colsum[colsums] << " ";
+		cout << colsum[colsums] << setw(5);
 	}
 
-	cout << totsum;
+	cout  << totsum;
 }
 
 int main()
